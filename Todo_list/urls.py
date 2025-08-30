41e35cf54b39d130fe6ed4f7c78c83a4659e2e23
Andriy125 <yaruta.andriy@gmail.com>
2025-08-30 14:18:02 +0300

    Initial TODO app

--- Todo_list/urls.py
+++ Todo_list/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for Todo_list_yaruyaru125 project.
+URL configuration for Todo_list project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
--- Todo_list/wsgi.py
+++ Todo_list/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Todo_list_yaruyaru125 project.
+WSGI config for Todo_list project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Todo_list_yaruyaru125.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Todo_list.settings')
 
 application = get_wsgi_application()
